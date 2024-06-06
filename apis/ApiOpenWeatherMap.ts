695357f2601460cc28f0cf9d8fd399ee9fdb8a8c
Yael Ruiz <yaelruiz99@outlook.com>
2024-06-06 09:53:35 -0600

    feat: ApiOpenWeatherMap and Axios dependence

---
+++ apis/ApiOpenWeatherMap.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const ApiOpenWeatherMap = axios.create({
+  baseURL: process.env.API_OPENWEATHERMAP_URL,
+  params: {
+    appid: process.env.API_OPENWEATHERMAP_API_KEY,
+  },
+});
+
+export default ApiOpenWeatherMap;
---
+++ apis/index.ts
@@ -0,0 +1,1 @@
+export { default as ApiOpenWeatherMap } from './ApiOpenWeatherMap';
